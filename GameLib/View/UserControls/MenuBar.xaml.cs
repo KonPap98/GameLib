814e3c55cdccf15a1373c2ecf48c46acd22a396a
KonPap98 <KonstantinosPapadopoulos98@gmail.com>
2025-04-17 21:53:36 +0300

    Add project files.

---
+++ GameLib/View/UserControls/MenuBar.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameLib.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for usew.xaml
+    /// </summary>
+    public partial class MenuBar : UserControl
+    {
+        public MenuBar()
+        {
+            InitializeComponent();
+        }
+
+        private void Searchbt_Click(object sender, RoutedEventArgs e)
+        {
+            SearchText(SearchBox.Text);
+        }
+        void SearchText(string text) => Trace.WriteLine("Searched for: " + text);
+    }
+}
